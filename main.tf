41ad51cf2c930b7c7c5cfbedb54c013faf179ced
vladskvortsov <vladskvortsov96@gmail.com>
2023-05-21 14:05:34 +0300

    vars added

--- main.tf
+++ main.tf
@@ -10,32 +10,30 @@ terraform {
 
 
 provider "aws" {
-  region     = "eu-west-2"
+  region     = var.aws_region
 
 }
-
 
 
 resource "aws_key_pair" "key" {
-  key_name   = "key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCdr1Gfzlq3kYLnx4keRGoxLOb6LtejdieKr4e02bvjGrR85RbLLQIylXGCvl7SR4sMWQYF4GvhjiaET5MnA/kR0++Gjs77CY4pZYdWyIrK2RAg1C5XWf4EcCbGrYbi2pm6WFE70MKRBPof/iU4L6Yd1LeIHf8Lm0EAIwXSaNYyxO8L+TUdJ2zjPbwg76WZjTx5mUVU+wjiaIob9nEDJ5soAyT6IJrek18ocO5Agot3aCc+aw0f8p+l4z2JJNUlSOVlDMdInmBJg7XqUXJ5kOClrATSdBj1KfTO+mY4uZNmjgB8x9WuKV3N2xCr6ksqwCKb9uzfWDgUS7o34vertuM6KKT4Lk0VNFcg8auGJCMqZXBcCUIMva/KELPRlB5ShTvlP9HvepLuptznPXcVGsOUiQv4qU1v/ZqBFPeT6ANi+BefX6kryhnTprfA3QTo6o4b1vdzgThT3EaogDg0cvozLCd8rTtWaHwCvaSK/kjzeGJHWxGseWLdReV1rHAv3C0= root@vlad-HP-ProBook-640-G1"
+  key_name   = var.key
+  public_key = var.public_key_cont
+
 }
-
-
 
 
 
 resource "aws_instance" "web" {
-  ami               = "ami-0eb260c4d5475b901"
+  ami               = var.image_id
 
   instance_type     = "t2.micro"
-  key_name               = "key"
-  vpc_security_group_ids = ["sg-00006d10243dc6666"]
+  key_name               = var.key
+  vpc_security_group_ids = var.security_group_id
   monitoring             = true
 
 
   tags = {
-    Name = "web"
+    Name = var.instance_name
     Terraform   = "true"
     Environment = "dev"
   }
@@ -69,7 +67,7 @@ resource "ansible_playbook" "playbook" {
   extra_vars = {
   ansible_host = aws_instance.web.public_ip
   ansible_ssh_user = "ubuntu"
-  ansible_ssh_private_key_file = "~/terraform-grafana/key.pem"
+  ansible_ssh_private_key_file = var.private_key
 
   }
 }
