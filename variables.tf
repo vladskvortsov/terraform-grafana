39991873e1c15beab00a35cf96b1d958d9e7aa5a
vlad <vladskvortsov96@gmail.com>
2023-06-13 01:23:12 +0300

    action addedB

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-image_id = "ami-0eb260c4d5475b901"
-instance_name = "web"
-public_key_cont = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCdr1Gfzlq3kYLnx4keRGoxLOb6LtejdieKr4e02bvjGrR85RbLLQIylXGCvl7SR4sMWQYF4GvhjiaET5MnA/kR0++Gjs77CY4pZYdWyIrK2RAg1C5XWf4EcCbGrYbi2pm6WFE70MKRBPof/iU4L6Yd1LeIHf8Lm0EAIwXSaNYyxO8L+TUdJ2zjPbwg76WZjTx5mUVU+wjiaIob9nEDJ5soAyT6IJrek18ocO5Agot3aCc+aw0f8p+l4z2JJNUlSOVlDMdInmBJg7XqUXJ5kOClrATSdBj1KfTO+mY4uZNmjgB8x9WuKV3N2xCr6ksqwCKb9uzfWDgUS7o34vertuM6KKT4Lk0VNFcg8auGJCMqZXBcCUIMva/KELPRlB5ShTvlP9HvepLuptznPXcVGsOUiQv4qU1v/ZqBFPeT6ANi+BefX6kryhnTprfA3QTo6o4b1vdzgThT3EaogDg0cvozLCd8rTtWaHwCvaSK/kjzeGJHWxGseWLdReV1rHAv3C0= root@vlad-HP-ProBook-640-G1"
-aws_region = "eu-west-2"
-key = "key"
-security_group_id  = ["sg-00006d10243dc6666"]
-private_key = "~/terraform-grafana/key.pem"
+variable "image_id" {}
+variable "instance_name" {}
+variable "public_key_cont" {}
+variable "aws_region" {}
+variable "key" {}
+variable "security_group_id" {}
+variable "private_key" {}
