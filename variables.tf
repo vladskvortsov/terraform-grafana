dad9fb9e5e005b1689963a77dd9ee584adac2787
vladskvortsov <vladskvortsov96@gmail.com>
2023-06-14 16:00:48 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,3 +4,12 @@ variable "aws_region" {
   default     = "us-west-2"
 }
 
+variable "image_id" {
+  type = string
+}
+variable "instance_name" {}
+variable "public_key_cont" {}
+variable "aws_region" {}
+variable "key" {}
+variable "security_group_id" {}
+variable "private_key" {}
