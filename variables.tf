1feee02cf9f1ae0ead747a8bd0e5a6ffe9134e90
vladskvortsov <vladskvortsov96@gmail.com>
2023-06-14 16:01:47 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "image_id" {
 }
 variable "instance_name" {}
 variable "public_key_cont" {}
-variable "aws_region" {}
+
 variable "key" {}
 variable "security_group_id" {}
 variable "private_key" {}
