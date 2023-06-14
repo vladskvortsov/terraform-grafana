e2f2aa0538a399627bca23804d763e311da9ca55
vladskvortsov <vladskvortsov96@gmail.com>
2023-06-14 13:42:22 +0300

    Delete variables.tf

--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "image_id" {}
-variable "instance_name" {}
-variable "public_key_cont" {}
-variable "aws_region" {}
-variable "key" {}
-variable "security_group_id" {}
-variable "private_key" {}
